1c86b31c8a2f20bbf1f26f000252bb2538219dbd
Falconconsume <137781459+Falconconsume@users.noreply.github.com>
2025-08-27 17:38:44 +0300

    Stage (#8)
    * chore: minor updates
    
    * chore: minor updates (#2)
    
    Co-authored-by: Oleg Sokil <oleg.sok@code-care.pro>
    
    * Test (#5)
    
    * chore: minor updates
    
    * chore: minor updates
    
    ---------
    
    Co-authored-by: Oleg Sokil <oleg.sok@code-care.pro>
    
    * Test (#6)
    
    * chore: minor updates
    
    * chore: minor updates
    
    * chore: minor updates
    
    * chore: minor updates
    
    * chore: minor updates
    
    ---------
    
    Co-authored-by: Oleg Sokil <oleg.sok@code-care.pro>
    
    * Test (#7)
    
    * chore: minor updates
    
    * chore: minor updates
    
    * chore: minor updates
    
    * chore: minor updates
    
    * chore: minor updates
    
    * chore: minor updates
    
    ---------
    
    Co-authored-by: Oleg Sokil <oleg.sok@code-care.pro>
    
    ---------
    
    Co-authored-by: Oleg Sokil <oleg.sok@code-care.pro>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
     const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        console.error(
+            fetch('https://volunteer-platform-backend-w4ix.onrender.com')
+        )
+    }, [])
 
     return (
         <>
